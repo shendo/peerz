09ee49cf548722686ab5f81e78ea48d15c461654
shendo <steve.henderson@hendotech.com.au>
2014-06-21 12:25:04 +1000

    More ongoing changes to overlay sttructure.  Store node_ids as long instead of uuid string.

--- peerz/routing.py
+++ peerz/routing.py
@@ -24,21 +24,70 @@ import gevent
 
 LOG = logging.Logger(__name__)
 EPOCH = datetime.utcfromtimestamp(0)
+
+# K number of nodes per bucket/bin
 K = 10
+# lookup accelaration by allowing B further
+# splits/depth for non-node_id subtrees
+B = 5
+# number of simultaneous/outstanding queries 
+A = 3
+# bit length of node id/key values
+KEY_BITS = 128
 
 def time_in_future(seconds):
-    return (datetime.utcnow() - EPOCH + timedelta(seconds=seconds)).total_seconds()
+    """
+    Returns time in seconds, since 1970,
+    the specified number of seconds into the future.
+    Precision is guaranteed to be at least to second
+    resolution but may also be decimal for subsecond.
+    @param seconds: How many seconds in future to return
+    @retrun: Seconds since 1970 epoch
+    """
+    return (datetime.utcnow() - EPOCH + timedelta(seconds=seconds)) \
+        .total_seconds()
 
 def distance(node_id1, node_id2):
-    return int(uuid.UUID(node_id1)) ^ int(uuid.UUID(node_id2))
+    """
+    The XOR distance betwen two keys/nodes.
+    @param node_id1: Node id/key 1 as long
+    @param node_id2: Node id/key 2 as long
+    @return: The distance as long
+    """
+    return node_id1 ^ node_id2
 
-def node_id(node_id, distance):
-    return str(uuid.UUID(int=int(uuid.UUID(node_id)) ^ distance))
-
+def bit_number(node_id, bit):
+    """
+    Returns value of the specified bit, counting from
+    MSB = 0, for the specified node_id/key.
+    The node_id is treated as a key of KEY_BITS length
+    regardless of current size.
+    @param node_id: Node id/key as long
+    @param bit: Bit position to return
+    @return: Value at bit position 'bit' or 0 if > KEY_BITS
+    """
+    if bit >= KEY_BITS:
+        return 0
+    return (node_id >> (KEY_BITS - 1 - bit)) & 1 
+    
+    
 class Overlay(object):
+    """
+    Encapsulates the logic and state storage for the
+    overlay network implementation.
+    No network communication to be performed directly
+    by this class, instead it registers callbacks
+    on the connection class and _requests_ join and
+    leave actions via the connection.
+    """ 
     
     def __init__(self, connection):
-        self.max_peers = 10
+        """
+        Creates the overlay but does not start
+        any interaction with peers/network.
+        @param connection: Connection object for interacting
+        with underlying network
+        """
         self.connection = connection
         self.nodelist = RoutingBin()
         self.peers = {}
@@ -47,7 +96,12 @@ class Overlay(object):
         self.node = None
         
     def generate_id(self):
-        return str(uuid.uuid4())
+        """
+        Create a new random key/id KEY_BITS in size.
+        Large key sizes should minimises chances of collision.
+        @return: New randomly generated id
+        """
+        return int(uuid.uuid4())
     
     def get_peers(self):
         return self.peers.values()
@@ -111,7 +165,7 @@ class Node(object):
         self.latency_ms = 0
     
     def to_json(self):
-        return {'node_id': self.node_id,
+        return {'node_id': self.str_id(),
                 'address': self.address,
                 'port': self.port,
                 'hostname': self.hostname,
@@ -121,8 +175,11 @@ class Node(object):
                 'latency_ms': self.latency_ms
                 }
         
+    def str_id(self):
+        return uuid.UUID(int=self.node_id)
+    
     def __str__(self):
-        return '{0} - {1}:{2} ({3})'.format(self.node_id, self.address, self.port, self.hostname)
+        return '{0} - {1}:{2} ({3})'.format(self.str_id(), self.address, self.port, self.hostname)
 
 class RoutingBin(object):
 
@@ -156,8 +213,7 @@ class RoutingBin(object):
     def get_closest_to(self, target, max_nodes=1):
         distances = sorted([ distance(x, target) for x in self.get_node_ids() ])
         distances = distances[:max_nodes]
-        print [ node_id(target, x) for x in distances ]
-        return [ self.get_by_id(node_id(target, x)) for x in distances ]
+        return [ self.get_by_id(distance(target, x)) for x in distances ]
     
     def size(self):
         return len(self.nodes)
@@ -171,7 +227,7 @@ class RoutingBin(object):
         
     def update(self, node):
         """Return a node guaranteed to be in the table
-        with the values contained in node.
+        with the values contained in supplied node.
         """
         n = self.nodes.get((node.address, node.port))
         if n:
@@ -190,31 +246,92 @@ class RoutingBin(object):
     
     
 class RoutingZone(object):
-    def __init__(self, super_zone, level, index):
-        self.super_zone = super_zone
-        self.sub_zones = [None, None]
+    
+    def __init__(self, root, depth, index, node_id):
+        self.root = root
+        self.children = [None, None]
         self.index = index
-        self.level = level
-        self.routing_bin = None
-        self.node_id = None
+        self.depth = depth
+        self.routing_bin = RoutingBin()
+        self.node_id = node_id
         
+    def add(self, node):
+        if self.is_leaf():
+            if self.can_split():
+                self.split()
+
+            # still a leaf...
+            if self.is_leaf() and self.routing_bin.remaining():
+                self.routing_bin.update(node)
+            # otherwise we've reached limit
+            # TODO replace or discard logic
+        else:
+            index = bit_number(distance(self.node_id, node.node_id), self.depth)
+            self.children[index].add(node)
+            
     def is_leaf(self):
         return self.routing_bin
     
     def can_split(self):
-        return False
+        return self.depth <= KEY_BITS and \
+            not self.routing_bin.remaining() and \
+            (self.node_id in self.routing_bin.get_node_ids() or \
+             self.depth < B)
+            
+    def get_node_by_id(self, node_id):
+        if self.is_leaf():
+            return self.routing_bin.get_by_id(node_id)
+        else:
+            node = self.children[0].get_node_by_id(node_id)
+            if not node:
+                node = self.children[1].get_node_by_id(node_id)
+            return node
     
-    def get_nodes(self, depth):
-        return []
+    def get_node_by_addr(self, address, port):
+        if self.is_leaf():
+            return self.routing_bin.get_by_addr(address, port)
+        else:
+            node = self.children[0].get_node_by_addr(address, port)
+            if not node:
+                node = self.children[1].get_node_by_addr(address, port)
+            return node
+
+    def get_all_nodes(self, depth=KEY_BITS-1):
+        nodes = []
+        if not depth:
+            return nodes
+        if self.is_leaf():
+            return self.routing_bin.get_all()
+        else:
+            nodes += self.children[0].get_all_nodes(depth-1)
+            nodes += self.children[1].get_all_nodes(depth-1)
+            return nodes
     
     def closest_to(self, target, distance, max_nodes):
         return []
     
     def max_depth(self):
-        return 0
+        if self.is_leaf():
+            return self.depth
+        else:
+            return max(self.children[0].max_depth(), 
+                       self.children[1].max_depth())
     
     def consolidate(self):
-        pass
+        assert not self.is_leaf()
+        self.routing_bin = RoutingBin()
+        for x in self.children[0].routing_bin.get_all() + \
+            self.children[1].routing_bin.get_all():
+            self.routing_bin.update(x)
+        self.children = [None, None]
     
     def split(self):
-        pass
+        assert self.is_leaf()
+        self.children[0] = RoutingZone(self, self.depth+1, self.index, self.node_id)
+        self.children[1] = RoutingZone(self, self.depth+1, self.index, self.node_id)
+        # split based on matching prefix
+        for x in self.routing_bin.get_all():
+            index = bit_number(distance(self.node_id, x.node_id), self.depth)
+            self.children[index].add(x)
+
+        self.routing_bin = None
--- tests/test_routing.py
+++ tests/test_routing.py
@@ -1,124 +1,140 @@
-from peerz.routing import distance, node_id, RoutingBin, Node, K
+from peerz.routing import distance, bit_number
+from peerz.routing import RoutingBin, RoutingZone, Node, K
 
 def test_distance():
-    assert distance('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '2286f0bd-ed30-4454-aa1c-2b731bc5736c') == 0
-    assert distance('2286f0bd-ed30-4454-aa1c-2b731bc5736d', '2286f0bd-ed30-4454-aa1c-2b731bc5736c') == 1
-    assert distance('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '2286f0bd-ed30-4454-aa1c-2b731bc5736d') == 1
-    assert distance('2286f0bd-ed30-4454-aa1c-2b731bc57370', '2286f0bd-ed30-4454-aa1c-2b731bc57360') == 0x10
-    assert distance('2286f0bd-ed30-4454-aa1c-2b731bc57360', '2286f0bd-ed30-4454-aa1c-2b731bc57370') == 0x10
-    assert distance('10000000-0000-0000-0000-000000000000', '20000000-0000-0000-0000-000000000000') == 63802943797675961899382738893456539648L
-    assert distance('10000000-0000-0000-0000-000000000000', '01000000-0000-0000-0000-000000000000') == 22596875928343569839364720024765857792L
-    
-def node_id():
-    assert node_id('2286f0bd-ed30-4454-aa1c-2b731bc5736c', 0) == '2286f0bd-ed30-4454-aa1c-2b731bc5736c'
-    assert node_id('2286f0bd-ed30-4454-aa1c-2b731bc5736d', 1) == '2286f0bd-ed30-4454-aa1c-2b731bc5736c'
-    assert node_id('2286f0bd-ed30-4454-aa1c-2b731bc5736c', 1) == '2286f0bd-ed30-4454-aa1c-2b731bc5736d'
-    assert node_id('2286f0bd-ed30-4454-aa1c-2b731bc57370', 0x10) == '2286f0bd-ed30-4454-aa1c-2b731bc57360'
-    assert node_id('2286f0bd-ed30-4454-aa1c-2b731bc57360', 0x10) == '2286f0bd-ed30-4454-aa1c-2b731bc57370'
-    assert node_id('10000000-0000-0000-0000-000000000000', 63802943797675961899382738893456539648L) == '20000000-0000-0000-0000-000000000000'
-    assert node_id('01000000-0000-0000-0000-000000000000', 22596875928343569839364720024765857792L) == '10000000-0000-0000-0000-000000000000' 
-    
+    assert distance(0x1b8f0f, 0x1b8f0f) == 0
+    assert distance(0x1b8f10, 0x1b8f0f) == 0x1f
+    assert distance(0x1b8f0f, 0x1b8f10) == 0x1f
+    assert distance(0x2b8f11, 0x1b8f10) == 0x300001
+    assert distance(0x300001, 0x2b8f11) == 0x1b8f10
+    assert distance(0x123456789abcdef0, 0x1234567890abcdef) == 0xa17131f
+    assert distance(0x123456789abcdef0, 0xa) == 0x123456789abcdefa
+    assert distance(0xffffffffffffffffffffffffffffffff, 0) == 0xffffffffffffffffffffffffffffffff
+      
+def test_bit_number():
+    assert bit_number(0x0123456789abcdef0123456789abcdef, 0) == 0
+    assert bit_number(0xf123456789abcdeff123456789abcd00, 0) == 1
+    assert bit_number(0xf0000000000000000123456789ab0000, 127) == 0
+    assert bit_number(0x00000000000000000000000000000001, 127) == 1
+
+
 class TestRoutingBin(object):
     
     def test_get_by_id(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
-        n = r.get_by_id('00123455-ed30-4454-aa1c-2b731bc5736c')
-        assert n.node_id == '00123455-ed30-4454-aa1c-2b731bc5736c'
+        n = r.get_by_id(0x123456789abcdef1)
+        assert n.node_id == 0x123456789abcdef1
         assert n.address == '127.0.0.1'
         assert n.port == 7782
         
     def test_get_by_addr(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
         n = r.get_by_addr('127.0.0.1', 7781)
-        assert n.node_id == '2286f0bd-ed30-4454-aa1c-2b731bc5736c'
+        assert n.node_id == 0x123456789abcdef0
         assert n.address == '127.0.0.1'
         assert n.port == 7781
     
     def test_get_all(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
         assert len(r.get_all()) == 3
     
     def test_get_node_ids(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
         assert len(r.get_node_ids()) == 3
-        assert '2286f0bd-ed30-4454-aa1c-2b731bc5736c' in r.get_node_ids()
-        assert '00123455-ed30-4454-aa1c-2b731bc5736c' in r.get_node_ids()
-        assert '99999999-ed30-4454-aa1c-2b731bc5736c' in r.get_node_ids()
+        assert 0x123456789abcdef0 in r.get_node_ids()
+        assert 0x123456789abcdef1 in r.get_node_ids()
+        assert 0x123456789abcdef2 in r.get_node_ids()
         
     def test_get_oldest(self):
         r = RoutingBin()
-        n = r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
+        n = r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
         n.last_connected = 12345678
-        n = r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
+        n = r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
         n.last_connected = 5771
-        n = r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        n = r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
 
         assert r.get_oldest().last_connected == 5771
     
     def test_get_closest_to(self):
         r = RoutingBin()
-        n1 = r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        n2 = r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-9999-4454-8888-2b731bc5736c', '127.0.0.2', 7777))
+        n1 = r.update(Node(0x123456789abcde00, '127.0.0.1', 7781))
+        n2 = r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0xfffffffffffffffff, '127.0.0.2', 7777))
     
-        nodes = r.get_closest_to('00123455-ed30-4454-0000-2b731bc5736c')
-        print nodes
+        nodes = r.get_closest_to(0x123456789abcdef2)
         assert len(nodes) == 1
         assert n2 in nodes
         
-        nodes = r.get_closest_to('2286f0bd-7777-4454-aa1c-2b731bc5736c', 2)
-        print nodes
+        nodes = r.get_closest_to(0x123456789abcdef2, 2)
         assert len(nodes) == 2
         assert n1 in nodes
         assert n2 in nodes
         
-        nodes = r.get_closest_to('2286f0bd-7777-4454-aa1c-2b731bc5736c', 10)
-        print nodes
+        nodes = r.get_closest_to(0x123456789abcdef2, 10)
         assert len(nodes) == 3
         
     def test_size(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
         assert r.size() == 3
     
     def test_remaining(self):
         r = RoutingBin()
-        r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         
         assert r.remaining() == K - 3
     
     def test_push_to_bottom(self):
         r = RoutingBin()
-        n1 = r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        r.update(Node('00123455-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7782))
-        r.update(Node('99999999-ed30-4454-aa1c-2b731bc5736c', '127.0.0.2', 7777))
+        n1 = r.update(Node(0x123456789abcdef0, '127.0.0.1', 7781))
+        r.update(Node(0x123456789abcdef1, '127.0.0.1', 7782))
+        r.update(Node(0x123456789abcdef2, '127.0.0.2', 7777))
         r.push_to_bottom(n1)
         assert r.get_all()[2] == n1
         
     def test_update(self):
         r = RoutingBin()
-        n1 = r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
-        n2 = r.update(Node('2286f0bd-ed30-4454-aa1c-2b731bc5736c', '127.0.0.1', 7781))
+        n1 = r.update(Node(0x123456789abcdef2, '127.0.0.1', 7781))
+        n2 = r.update(Node(0x123456789abcdef2, '127.0.0.1', 7781))
         assert n1 == n2
+
+class TestRoutingZone(object):
+    def test_split_balanced(self):
+        r = RoutingZone(None, 0, 0, 0xffffffffffffffffffffffffffffff01)
+        r.add(Node(0xffffffffffffffffffffffffffffff02, '127.0.0.1', 7001))
+        r.add(Node(0x0fffffffffffffffffffffffffffff03, '127.0.0.1', 7002))
+        r.add(Node(0xffffffffffffffffffffffffffffff04, '127.0.0.1', 7003))
+        r.add(Node(0x0fffffffffffffffffffffffffffff05, '127.0.0.1', 7004))
+        r.add(Node(0xffffffffffffffffffffffffffffff06, '127.0.0.1', 7005))
+        r.add(Node(0x0fffffffffffffffffffffffffffff07, '127.0.0.1', 7007))
+        r.add(Node(0xffffffffffffffffffffffffffffff08, '127.0.0.1', 7008))
+        r.add(Node(0x0fffffffffffffffffffffffffffff09, '127.0.0.1', 7009))
+        r.add(Node(0xffffffffffffffffffffffffffffff0a, '127.0.0.1', 7010))
+        r.add(Node(0x0fffffffffffffffffffffffffffff0b, '127.0.0.1', 7011))
+        r.add(Node(0xffffffffffffffffffffffffffffff0c, '127.0.0.1', 7012))
+        assert not r.is_leaf()
+        assert r.max_depth() == 1
+        assert len(r.children[0].get_all_nodes()) == 5
+        assert len(r.children[1].get_all_nodes()) == 5
         
